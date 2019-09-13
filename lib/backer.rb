32937f8c0517197d3971b53bd3f1d15972e40c09
Mars Le <hp.mars.le@gmail.com>
2019-09-13 00:16:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,12 +2,9 @@ class Backer
   attr_accessor :name
   attr_reader :backed_projects
   
-  # @@all = []
-  
   def initialize(name)
     @name = name
     @backed_projects = []
-    # @@all << self 
   end 
   
   def back_project(project)
